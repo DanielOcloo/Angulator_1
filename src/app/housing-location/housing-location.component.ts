8ce61fab02d54d8a41fceac61535ef7c54ecfe5c
DanielOcloo <ynnadocloo@gmail.com>
2024-01-09 14:21:16 +0000

    Added new pages and routing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Housinglocation } from '../housinglocation';
 import { Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
